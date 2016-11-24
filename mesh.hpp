954e2f946e4bbcfb862d9732dc332d12c44c77c3
Giovanni Nuño <gionuno@cimat.mx>
2016-11-24 14:34:05 -0600

    Update mesh.hpp

--- mesh.hpp
+++ mesh.hpp
@@ -4,14 +4,12 @@
 #include <GL/glew.h>
 #include <GL/gl.h>
 
-#include <armadillo>
 #include <glm/glm.hpp>
 
 #include <fstream>
 #include <sstream>
 #include <string>
 
-using namespace arma;
 using namespace std;
 
 struct screen
@@ -85,75 +83,5 @@ struct screen
         glDisableVertexAttribArray(0);
     }
 };
-/*
-struct mesh
-{
-    GLuint i_vbo;
-    GLuint x_vbo;
-    GLuint n_vbo;
 
-    mesh(const string & filename)
-    {
-        vector<int> e;
-
-        vector<vec3> x;
-        vector<vec3> n;
-
-        vector<vector<int> > f_x;
-        vector<vector<int> > f_n;
-
-        ifstream file(filename.c_str());
-        string s;
-        while(getline(file,s))
-        {
-            if(s.size()<3)
-                continue;
-            stringstream ss(s.substr(2));
-            if(s.substr(0,2) == "v ")
-            {
-                vec3 aux;
-                ss >> aux.x >> aux.y >> aux.z;
-                x.push_back(aux);
-            }
-            else if(s.substr(0,2) == "vn")
-            {
-                vec3 aux;
-                ss >> aux.x >> aux.y >> aux.z;
-                n.push_back(aux);
-            }
-            else if(s.substr(0,2) == "f ")
-            {
-                int a_x,b_x,c_x;
-                int a_n,b_n,c_n;
-
-                char aux;
-
-                ss >> a_x >> aux >> aux >> a_n;
-                ss >> b_x >> aux >> aux >> b_n;
-                ss >> c_x >> aux >> aux >> c_n;
-
-                f_x.push_back(vector<int>(3));
-                f_x[f_x.size()-1][0] = a_x-1;
-                f_x[f_x.size()-1][1] = b_x-1;
-                f_x[f_x.size()-1][2] = c_x-1;
-
-                f_n.push_back(vector<int>(3));
-                f_n[f_n.size()-1][0] = a_n-1;
-                f_n[f_n.size()-1][1] = b_n-1;
-                f_n[f_n.size()-1][2] = c_n-1;
-            }
-        }
-
-        vector<GLuint> e;
-        for(auto & )
-    }
-
-    ~mesh()
-    {
-        glDeleteBuffers(1,&i_vbo);
-        glDeleteBuffers(1,&x_vbo);
-        glDeleteBuffers(1,&n_vbo);
-    }
-};
-*/
 #endif // MESH_HPP_INCLUDED
